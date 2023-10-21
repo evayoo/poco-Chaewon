76d3c94963236babc17e6ed8488316e27f0e8ba9
Chaewon <chaewon.yoo@powers.org>
2023-10-21 21:49:58 +0200

    update dom

--- Week5/script4.js
+++ Week5/script4.js
@@ -73,3 +73,19 @@ console.log(Soup.myRecipe());
 
 // 
 
+function ageCalculator(){        
+    let thisYear = new Date().getFullYear();
+    let workYear = retireAge - myAge
+    if (myAge <= 0 || retireAge <= 0){    
+        let myAge = +prompt("What is your age?")
+    let retireAge = +prompt("When do you retire?")
+    }
+    else if (myAge > retireAge)
+    {return `You already retired.` }
+    else (myAge <= retireAge)
+    {
+        
+        return `You have ${workYear} left until you can retire. 
+        It's ${thisYear}, so you can retire in ${thisYear + workYear}` }
+}
+ageCalculator();
--- Week5/script7.js
+++ Week5/script7.js
@@ -16,3 +16,18 @@ arr.forEach(function(el){
     el.textContent = "new text";
 });
 
+let urlDisplayElement = document.getElementById('urlDisplay');
+let currentUrl = window.location;
+urlDisplayElement.innerHTML = `Current url is: ${currentUrl}`
+currentUrl.replace(urlDisplayElement, h3), 
+currentUrl.textContent = "this would be rendered as html"
+
+// let object = {
+//     "someProperty": "something"
+//      "someOtherProperty": "somethingElse"
+// }
+
+let userInput = document.getElementById('input').value;
+
+document.getElementById('output').innerText = eval("object." + userInput)
+.something; alert('hacked');
